046f7635b24ff274903308ced32305c011fcbd8d
unknown <1343008086@qq.com>
2026-08-08 12:22:58 +0900

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "statusbar.lyric"
         minSdk = 30
         targetSdk = 36
-        versionCode = 106
-        versionName = "1.0.7"
+        versionCode = 107
+        versionName = "1.1.0"
         buildConfigField("long", "BUILD_TIME", "${buildTime}L")
         buildConfigField("int", "COMPOSE_CONFIG_VERSION", "1")
         setProperty("archivesBaseName", "StatusBarLyric_Next-$versionName($versionCode)")
